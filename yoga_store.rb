738eeb99053a9ed12a36a4d4e145f9e297451d0e
svanhesteren <s.vanhesteren@gmail.com>
2017-12-06 15:53:51 +0100

    Departments added

--- yoga_store.rb
+++ yoga_store.rb
@@ -1,2 +1,34 @@
 
 # start
+@shopping_cart = []
+
+@departments = [ :mats, :props, :clothes, :books ]
+
+@products = {
+  mats: [
+    { reference_number: 1231, name: "Super Lite Mat", price: 10 },
+    { reference_number: 1232, name: "Power Mat", price: 20 },
+  ],
+  props: [
+    { reference_number: 1233, name: "Block", price: 30 },
+    { reference_number: 1234, name: "Meditation cushion", price: 30 },
+  ],
+  clothes: [
+    { reference_number: 1235, name: "The best T-shirt", price: 200 },
+    { reference_number: 1236, name: "The cutest yoga pants", price: 300 },
+  ],
+  books: [
+    { reference_number: 1237, name: "Bring Yoga To Life", price: 30 },
+    { reference_number: 1238, name: "Light On Yoga", price: 10 },
+  ]
+}
+
+puts "Welcome to our store!"
+puts "Our currents departments are: "
+puts
+@departments.each_with_index{|department, index|
+  puts "#{index+1}) #{department}"
+}
+puts
+puts "Select a department you wish to visit: "
+chosen_department = gets.to_i
